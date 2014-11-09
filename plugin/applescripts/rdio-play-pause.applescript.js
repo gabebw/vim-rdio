555a298b38c42c594c1fc7989c73a86dbb470bdc
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-09 16:15:48 -0500

    Reliably find Rdio tab

--- plugin/applescripts/rdio-play-pause.applescript.js
+++ plugin/applescripts/rdio-play-pause.applescript.js
@@ -7,7 +7,10 @@ function findRdioTab(){
   // Find the tab that has Rdio in it
   for(var i = 0; i < app.windows().length; i++){
     var window = app.windows[i];
-    var possibleRdioTabs = window.tabs.whose({ title: { _endsWith: 'Rdio' } })
+    var possibleRdioTabs = window.tabs.whose({
+      url: { _contains: "rdio.com" },
+      title: { _endsWith : "– Rdio" }
+    })
     if( possibleRdioTabs.length > 0 ){
       rdioTab = possibleRdioTabs.at(0);
       break;
