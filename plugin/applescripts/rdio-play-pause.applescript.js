a2343710c9de07879b753922a36459d784306da6
Gabe Berke-Williams <gabebw@gabebw.com>
2015-06-28 17:07:30 -0400

    Regenerate files

--- plugin/applescripts/rdio-play-pause.applescript.js
+++ plugin/applescripts/rdio-play-pause.applescript.js
@@ -2,29 +2,75 @@
 
 var VimRdio = VimRdio || {};
 
-VimRdio.findRdioTab = function(){
-  var app = Application("Google Chrome");
-  var rdioTab = undefined;
+// Detect if an application with the given name is running.
+// This is necessary because calling `Application(name)` causes that application
+// to open. This way we can check if something is running without opening it.
+VimRdio._applicationIsRunning = function(applicationName){
+  var systemEvents = Application("System Events");
 
-  // Find the tab that has Rdio in it
-  for(var i = 0; i < app.windows().length; i++){
-    var window = app.windows[i];
-    var possibleRdioTabs = window.tabs.whose({
-      url: { _contains: "rdio.com" },
-      title: { _endsWith : "Rdio" }
-    })
+  var applicationsWithName = systemEvents.applicationProcesses.whose({name: applicationName});
+  return applicationsWithName.length > 0;
+}
+
+// Given the name of an application (like "Google Chrome"), and the name of the
+// tab property that corresponds to the tab's title, search that application for
+// the tab that has Rdio playing in it.
+// We need tabTitleProperty because Chrome has `tab.title`, while Safari calls
+// it `tab.name`.
+VimRdio._findRdioTab = function(applicationName, tabTitleProperty){
+  var application = Application(applicationName);
+  var rdioTab = undefined;
+  var criteria = {};
+  criteria.url = { _contains: "rdio.com" };
+  criteria[tabTitleProperty] = { _endsWith : "Rdio" };
+
+  for(var i = 0; i < application.windows().length; i++){
+    var window = application.windows[i];
+    var possibleRdioTabs = window.tabs.whose(criteria);
     if( possibleRdioTabs.length > 0 ){
       rdioTab = possibleRdioTabs.at(0);
       break;
     }
   }
-  return rdioTab;
+  if(rdioTab){
+    return new VimRdio.RdioTab(application, rdioTab);
+  } else {
+    return undefined;
+  }
 }
+
+// Find a tab that has Rdio in it, in either Google Chrome or Safari.
+VimRdio.findRdioTab = function(){
+  if(VimRdio._applicationIsRunning("Google Chrome")){
+    return VimRdio._findRdioTab("Google Chrome", "title")
+  } else if(VimRdio._applicationIsRunning("Safari")){
+    return VimRdio._findRdioTab("Safari", "name")
+  }
+}
+
+// An object that wraps the Rdio browser tab returned by the Javascript bridge.
+//
+// It's an intermediate layer so we can use the same interface to send
+// Javascript to a Safari or Google Chrome tab, both of which have slightly
+// differnt APIs.
+
+VimRdio.RdioTab = function(application, tab){
+  this.application = application;
+  this.tab = tab;
+};
+
+VimRdio.RdioTab.prototype.executeJavascript = function(javascript){
+  if(this.application.name() === "Google Chrome"){
+    this.tab.execute({javascript: javascript});
+  } else if(this.application.name() === "Safari"){
+    this.application.doJavaScript(javascript, {in: this.tab});
+  }
+};
 
 
 function run(argv) {
   var rdioTab = VimRdio.findRdioTab();
-  var defineFunctions = "var VimRdio = VimRdio || {};  VimRdio.getPlaylistNames = function(){   return _.map($(\"a.playlist\"), function(a) { return $(a).prop(\"title\"); }) };  VimRdio.next = function(){   $(\"button.next\").click(); };  VimRdio.playCurrentPlaylist = function(){   $(\".PlayButton:visible:first\").click(); };  VimRdio.playFavorites = function(){   setTimeout(VimRdio.playCurrentPlaylist, 3000);    var $linkToFavoritesStation = $(\".user_nav .station_row\");   $linkToFavoritesStation.click(); };  VimRdio.playPause = function(){   $(\".play_pause\").click(); };  VimRdio.selectAndPlayPlaylist = function(playlistName){   setTimeout(VimRdio.playCurrentPlaylist, 3000);    $(\"a.playlist[title='\" + playlistName + \"']\").click() }";
-  rdioTab.execute({javascript: defineFunctions});
-  rdioTab.execute({javascript: "VimRdio.playPause()"});
+  var defineFunctions = "var VimRdio = VimRdio || {};  VimRdio.getPlaylistNames = function(){   return _.map($(\"a.playlist\"), function(a) { return $(a).prop(\"title\"); }) };  VimRdio.next = function(){   R.player.next(); };  VimRdio.playCurrentPlaylist = function(){   $(\".PlayButton:visible:first\").click(); };  VimRdio.playFavorites = function(){   setTimeout(VimRdio.playCurrentPlaylist, 3000);    var $linkToFavoritesStation = $(\".user_nav .station_row\");   $linkToFavoritesStation.click(); };  VimRdio.playPause = function(){   R.player.playPause() };  VimRdio.selectAndPlayPlaylist = function(playlistName){   setTimeout(VimRdio.playCurrentPlaylist, 3000);    $(\"a.playlist[title='\" + playlistName + \"']\").click() }";
+  rdioTab.executeJavascript(defineFunctions);
+  rdioTab.executeJavascript("VimRdio.playPause()");
 }
